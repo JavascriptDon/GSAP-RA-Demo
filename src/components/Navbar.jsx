dbb3eec42d0b2e405f4c6bf7dd00f1d7cdc3ea29
Helitha Rupasinghe <101202952+JavascriptDon@users.noreply.github.com>
2025-07-29 08:06:06 +0100

    Update Navbar.jsx to include nologo.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,6 @@ const Navbar = () => {
     <nav>
         <div>
             <a href="#home" className='flex items-center gap-2'>
-                <img src="/images/logo.png" alt="logo"/>
                 <p>RapidAuthor</p>
             </a>
 
