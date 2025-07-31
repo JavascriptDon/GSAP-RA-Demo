a77fb838b762e6d718247665f39a7cee98338e0e
Helitha Rupasinghe <hr21don>
2025-07-31 23:45:00 +0100

    implement art.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
+import Art from './components/Art';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Navbar/>
         <Hero/>
         <Features/>
+        <Art/>
     </main>
   )
 }
